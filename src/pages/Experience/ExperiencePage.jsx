cb2bca3d439b653dea051bbf9b7e09b27d185075
Luis Vasquez <Vluis5457@gmail.com>
2024-07-03 08:11:41 -0600

    Changed experience card order

--- src/pages/Experience/ExperiencePage.jsx
+++ src/pages/Experience/ExperiencePage.jsx
@@ -7,6 +7,37 @@ const ExperiencePage = () => {
         <h3 className="section-title">WORK EXPERIENCE</h3>
         <div className="container skill-container">
           <div className="skill">
+            <div className="skill-title">.NET Back-end developer</div>
+            <div className="skill-company">
+              <i className="bold">Integra International Services</i> -
+              <span className="bold">May 2024 - Present</span>
+            </div>
+            <div className="skill-description pt-3">
+              <p>
+                ✅ Development and maintenance of functionalities for a web
+                system using ASP .NET C# WebApi.
+              </p>
+              <p>✅ Bug fixing and integration of new functionalities.</p>
+              <p>
+                ✅ Adapting functionalities for new modules according to client
+                requirements.
+              </p>
+              <p>✅ Integration of SOAP web services with .NET.</p>
+              <p>
+                ✅ Creating and managing background jobs using Hangfire for
+                efficient task processing.
+              </p>
+              <p>
+                ✅ Implementing message-based communication and task queuing
+                with RabbitMQ to enhance system scalability and reliability.
+              </p>
+              <p>
+                ✅ Efficient code management and versioning using Git under
+                Scrum methodology.
+              </p>
+            </div>
+          </div>
+          <div className="skill">
             <div className="skill-title">.NET Developer</div>
             <div className="skill-company">
               <i className="bold">Creativa Consultores S.A.</i> -
@@ -38,37 +69,6 @@ const ExperiencePage = () => {
               <p>
                 ✅ Experience in reporting using ClosedXML and RDLC for
                 generating reports.
-              </p>
-            </div>
-          </div>
-          <div className="skill">
-            <div className="skill-title">.NET Back-end developer</div>
-            <div className="skill-company">
-              <i className="bold">Integra International Services</i> -
-              <span className="bold">May 2024 - Present</span>
-            </div>
-            <div className="skill-description pt-3">
-              <p>
-                ✅ Development and maintenance of functionalities for a web
-                system using ASP .NET C# WebApi.
-              </p>
-              <p>✅ Bug fixing and integration of new functionalities.</p>
-              <p>
-                ✅ Adapting functionalities for new modules according to client
-                requirements.
-              </p>
-              <p>✅ Integration of SOAP web services with .NET.</p>
-              <p>
-                ✅ Creating and managing background jobs using Hangfire for
-                efficient task processing.
-              </p>
-              <p>
-                ✅ Implementing message-based communication and task queuing
-                with RabbitMQ to enhance system scalability and reliability.
-              </p>
-              <p>
-                ✅ Efficient code management and versioning using Git under
-                Scrum methodology.
               </p>
             </div>
           </div>
